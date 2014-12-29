deffbc1ac18a23e83689d695da8632eb96a7b714
Ian Whitney <whit0694@umn.edu>
2014-12-29 12:20:15 -0600

    State rules, use delegation to meet the first rule
    In this refactoring, I'm going to follow the rules of East Oriented
    Code (details here
    http://saturnflyer.com/blog/jim/2014/12/23/enforcing-encapsulation-with-east-oriented-code/
    and here
    http://www.confreaks.com/videos/4825-RubyConf2014-eastward-ho-a-clear-path-through-ruby-with-oo)
    
    Just for the sake of all of our memories, I've put the rules at the top
    of the code.
    
    The first rule is to return self. Now, the `convert` method can't
    return self because it's a class method. But I'm going to treat
    `convert` as a declarative builder
    (http://programming.ianwhitney.com/blog/2014/04/13/4-simple-rules-and-declarative-builders/)
    meaning that I expect it to return an instance of Raindrops.
    
    To do that, I need to be able to instantiate instances of Raindrops and
    have them be string-like enough that the tests will pass. I'm going to
    do that with simple delegator. I do not need to explicitly override the
    initialize method, but I'm doing so to illustrate what's going on and to
    show that I am explicitly returning self

--- raindrops.rb
+++ raindrops.rb
@@ -1,12 +1,21 @@
 require 'prime'
 
-class Raindrops
+# 1) Always return self
+# 2) Objects can query themselves
+# 3) Factories are exempt
+class Raindrops < SimpleDelegator
   def self.convert(number)
     factors = Prime.prime_division(number).map {|x| x[0]}
-    ret = ""
-    ret << "Pling" if factors.include?(3)
-    ret << "Plang" if factors.include?(5)
-    ret << "Plong" if factors.include?(7)
-    ret.empty? ? number.to_s : ret
+    sound = ""
+    sound << "Pling" if factors.include?(3)
+    sound << "Plang" if factors.include?(5)
+    sound << "Plong" if factors.include?(7)
+    sound = number.to_s if sound.empty?
+    self.new(sound)
+  end
+
+  def initialize(sound)
+    __setobj__(sound)
+    self
   end
 end
