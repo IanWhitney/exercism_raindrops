6c7825802a85c6269e30fff209d19ae99e27e806
Ian Whitney <whit0694@umn.edu>
2014-12-29 11:04:53 -0600

    Initial passing version

---
+++ raindrops.rb
@@ -0,0 +1,12 @@
+require 'prime'
+
+class Raindrops
+  def self.convert(number)
+    factors = Prime.prime_division(number).map {|x| x[0]}
+    ret = ""
+    ret << "Pling" if factors.include?(3)
+    ret << "Plang" if factors.include?(5)
+    ret << "Plong" if factors.include?(7)
+    ret.empty? ? number.to_s : ret
+  end
+end
